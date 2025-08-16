95618ccc555f0f707437245798c96b587950c23e
Tejas Joshiii <tejasjoshi789@gmail.com>
2025-08-16 13:42:25 +0530

    fixed some error in admin logo due to not rendering it

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("Home.html")
+
+@app.route("/admin")  # URL error isliye aya admin ko register karna padega agar urlfor ka jinja tag use kar rha ho
+def admin():           # Function name
+    return render_template("Admin.html")
 
 # Run the application if this script is executed directly
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,3 +7,9 @@ os.makedirs("db", exist_ok=True)  # ensure folder exists
 
 db_url = os.getenv("db_connection")
 engine = create_engine(db_url, echo=True)
+
+def show_jobs():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        for row in result.all():
+            print(dict(row._mapping))
