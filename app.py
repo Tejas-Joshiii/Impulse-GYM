390825cfa4806fb2796d837d1d89f6b48a1c6fef
Tejas Joshiii <tejasjoshi789@gmail.com>
2025-08-31 01:42:54 +0530

    Now membershipdetails will stored at database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for
-from database import load_workout_from_db, init_db, load_workouts_from_db  # Import the function to load workouts from the database
+from flask import Flask, render_template, url_for, request, jsonify
+from database import load_workout_from_db, init_db, load_workouts_from_db, add_membershipTodb  # Import the function to load workouts from the database
 
 init_db() # yeh function initializes the database and creates the table if it doesn't exist and also create .db file but not db folder
 print("Database initialized ✅")
@@ -25,13 +25,39 @@ def list_fitness_programs():
 def admin():           # Function name
     return render_template("Admin.html")
 
-@app.route("/excercisePage/<id>")
+# <id> isme value home ke loop mei applyChartCard.html se ayi hai work in works ke through value work['id'] 
+@app.route("/exercisePage/<id>") # yeh apply karne ke baad wala page hai jisme us particular exercise ka info hai phir button click karne ke baad form aata hai 
 def showExcercise(id):
     worked = load_workouts_from_db(id) # workout and workouts different hai
     if not worked:
         return "Not Found", 404
     else:
         return render_template("exercisePage.html", company_name='Impulse', worked=worked)
+
+@app.route("/api/exercisePage/<id>")
+def list_fitness_json(id):
+    worked = load_workouts_from_db(id)
+    return jsonify(worked)
+
+@app.route("/applicationForm/<id>")
+def loadApplicationPage(id):
+    load = load_workouts_from_db(id)
+    if not load:
+        return "Form not found", 404
+    else:
+        return render_template("exerciseApplicationForm.html",work=load)
+
+@app.route("/exercisePage/<mem_id>/submit_membership", methods=["POST"]) # id jagah kux aur naam dedo mem_id mei value work['id'] se pass hogi variable ka kaam kar rha hai jo bhi <id> hai inme kahi na kahi se value pass ho rhi hai
+def feed_membership(mem_id):
+    data = request.form # request.args bhi data leta hai but data url mei visible hota hai yeh data ko browser requests directly url ko post karta hai aur data access hota request.form se
+    work = load_workouts_from_db(mem_id)
+    # print(dict(request.form))
+    add_membershipTodb(mem_id, data)
+    # return "Membership added successfully ✅"
+    # data = request.args   # yeh output Flask console me aana chahiye
+    # return jsonify(data
+    return render_template("formSubmitted.html", application=data, working=work)
+
 
 # Run the application if this script is executed directly
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -9,16 +9,27 @@ db_url = os.getenv("db_connection")
 engine = create_engine(db_url, echo=True)
 
 def init_db():
-    with engine.connect() as conn:
+    with engine.begin() as conn:
         conn.execute(text("""
             CREATE TABLE IF NOT EXISTS exercise (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT,
                 quote TEXT,
-                detail INT
+                detail TEXT
             )
         """))
-        conn.commit()
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS membership (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                mem_id INT,
+                fullName TEXT NOT NULL,
+                emailId TEXT NOT NULL,
+                phoneNo TEXT,
+                address TEXT,
+                membershipType TEXT
+            )
+        """))
+
 
 def load_workout_from_db():
     with engine.connect() as conn:
@@ -32,9 +43,22 @@ def load_workouts_from_db(id):
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM exercise WHERE id = :val"), {"val": id})
         rows = result.all()
-        if len(rows) == 0:
+        if len(rows) == 0: # agar list empty ho toh
             return None
         else:
             return rows[0]._asdict() # is list ka pehla tuple return kar raha hai us id no ki information
 
-            
+def add_membershipTodb(mem_id, data):
+    with engine.begin() as conn:
+        query = text("""INSERT INTO membership (mem_id, fullName, emailId, phoneNo, address, membershipType) VALUES (:mem_id, :fullName, :emailId, :phoneNo, :address, :membershipType)""")
+        conn.execute(query,
+                    {
+                        'mem_id' : mem_id,
+                        'fullName' : data['fullName'],
+                        'emailId'  : data['emailId'],
+                        'phoneNo'  : data['phoneNo'],
+                        'address'  : data['address'],
+                        'membershipType' : data['membershipType']
+                }) # conn.execute ham pakra deta hai in variable me data through data['fullName'] then insert into wale m values m data hota hai na ki koi class ya css se relation hai
+
+print("Using DB file at:", os.path.abspath("db/mydb.db"))
